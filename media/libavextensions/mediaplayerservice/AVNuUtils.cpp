c6575b045b5efb778ea361ddf47d54f12fc79ae9
Shalaj Jain <shalajj@codeaurora.org>
2015-10-13 17:56:23 -0700

    nuplayer: Drop frames with DATACORRUPT flag
    Drop frames in NuplayerDecoder if the corresponding
    OMX_BUFFERFLAG_DATACORRUPT is set from OMX.
    
    Add a util function to easily switch off this feature.
    
    Do not declare MediaSource explicitly.
    
    Change-Id: I97bd238d52d660e1ab61eac222cdff28334cc5cc
    
    Conflicts:
    	media/libavextensions/mediaplayerservice/AVNuExtensions.h

--- media/libavextensions/mediaplayerservice/AVNuUtils.cpp
+++ media/libavextensions/mediaplayerservice/AVNuUtils.cpp
@@ -154,6 +154,8 @@ bool AVNuUtils::canUseSetBuffers(const sp<MetaData> &/*Meta*/) {
     return false;
 }
 
+bool AVNuUtils::dropCorruptFrame() { return false; }
+
 // ----- NO TRESSPASSING BEYOND THIS LINE ------
 AVNuUtils::AVNuUtils() {}
 
