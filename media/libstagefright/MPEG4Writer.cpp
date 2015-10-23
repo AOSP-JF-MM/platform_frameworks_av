CyanogenMod/android_frameworks_av

MPEG4Writer: Fix MediaServer crash in MM Stress test.
  - Media Server crash is happening while accessing
    codecspecific data at the end of recording since
    stopRecording is issued from script even before
    codec spefic data is returned from encoder.
  - Size check is added to prevent accessing
    codecspecific data in this case.

 CRs-Fixed: 732893

Change-Id: I77ce3c02c76c4d3cc4345d761ad505090ef29530

media/libstagefright/MPEG4Writer.cpp
@@ -2699,6 +2699,11 @@ void MPEG4Writer::Track::writeTrackHeader(bool use32BitOffset) {
     ALOGV("%s track time scale: %d",
         mIsAudio? "Audio": "Video", mTimeScale);
 
+    if (mMdatSizeBytes == 0) {
+      ALOGV("Track data is not available.");
+      return;
+    }
+
     uint32_t now = getMpeg4Time();
     mOwner->beginBox("trak");
         writeTkhdBox(now);

AOSP-JF-MM/platform_frameworks_av

media/libstagefright/MPEG4Writer.cpp
@@ -2734,6 +2734,11 @@ void MPEG4Writer::Track::writeTrackHeader(bool use32BitOffset) {
     ALOGV("%s track time scale: %d",
         mIsAudio? "Audio": "Video", mTimeScale);
 
+    if (mMdatSizeBytes == 0) {
+        ALOGW("Track data is not available.");
+        return;
+    }
+
     uint32_t now = getMpeg4Time();
     mOwner->beginBox("trak");
         writeTkhdBox(now);